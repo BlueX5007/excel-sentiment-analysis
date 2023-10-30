c31e94fdde5d56a8e2b776146cd1d5d5352a128f
BlueX5007 <92089163+BlueX5007@users.noreply.github.com>
2023-10-30 11:27:17 +0000

    Added column mechanism

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ nltk.download('vader_lexicon')
 
 fileName = input("Enter the excel file name without the extension.")
 sheetName = input("Enter your sheet name.")
+columnNumber = input("Enter the column number which will be reserved for review analysis.")
+warning = input("By pressing Enter to continue, you hereby acknowledge that you are responsible for your files and that you have read how this code works and modifies your Excel file. Create a backup just for safety.")
 
 wb = openpyxl.load_workbook(f'{fileName}.xlsx')
 sheet = wb[sheetName]  # Replace with your sheet name
 
 sia = SentimentIntensityAnalyzer()
 
-for row in range(2, sheet.max_row + 1):  # Assuming row 1 contains headers
+for row in range(columnNumber, sheet.max_row + 1):  # Assuming row 1 contains headers
     review = sheet.cell(row=row, column=2).value  # Assuming reviews are in column 2
     polarity_scores = sia.polarity_scores(review)
     sentiment = 'neutral'
